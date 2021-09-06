1351e27101a27b090d5305387fa41e61a5df07ac
Elmabi <home@home.com>
2021-09-05 23:09:43 -0400

    Programm toggling students scores

--- src/components/StudentInfo.js
+++ src/components/StudentInfo.js
@@ -1,7 +1,12 @@
+import { useReducer } from "react";
 import "./studentInfo.css";
 
 export function StudentInfo({ student, grades }) {
   let gradesToNumbers = grades.map((i) => Number(i));
+  const [toggleScores, setToggleScores] = useReducer(
+    (toggleScores) => !toggleScores,
+    true
+  );
   return (
     <div>
       <li className="span-container">
@@ -9,7 +14,11 @@ export function StudentInfo({ student, grades }) {
         <div>
           <span>
             {student.firstName.toUpperCase()} {student.lastName.toUpperCase()}
+            <button onClick={setToggleScores}>{`${
+              toggleScores ? "+" : "-"
+            }`}</button>
           </span>
+
           <span className="padding-left">Email : {student.email}</span>
           <span className="padding-left">Company : {student.company} </span>
           <span className="padding-left">Skill : {student.skill} </span>
@@ -18,6 +27,12 @@ export function StudentInfo({ student, grades }) {
             {gradesToNumbers.reduce((a, b) => a + b, 0) / grades.length}
             {"%"}
           </span>
+
+          {gradesToNumbers.map((grade, id) => (
+            <span style={{ display: `${toggleScores ? "none" : "block"}` }}>
+              Test {id++ + 1} : {grade}%
+            </span>
+          ))}
         </div>
       </li>
       <span className="line"></span>
